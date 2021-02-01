cf7aba3f3a4184373d0eb133d591da2cce74b6c3
Ruslan4444 <ruslankv4@gmail.com>
2021-02-01 19:10:35 +0100

    Виконано завдання 1 - інший спосіб

--- js/task-1.js
+++ js/task-1.js
@@ -1,16 +1,26 @@
 
-const totalCategories = document.querySelectorAll('.item');
+// const totalCategories = document.querySelectorAll('.item');
 
-console.log(`В списке ${totalCategories.length} категории.`);
+// console.log(`В списке ${totalCategories.length} категории.`);
 
 
-const categoriesArray = [...totalCategories]
-    .map(categories => `Категория: ${categories.children[0].textContent}
-    Количество елементов: ${categories.children[1].children.length}`)
-    .join("\n");
+// const categoriesArray = [...totalCategories]
+//     .map(categories => `Категория: ${categories.children[0].textContent}
+//     Количество елементов: ${categories.children[1].children.length}`)
+//     .join("\n");
   
-console.log(categoriesArray);
+// console.log(categoriesArray);
     
+const itemRef = document.querySelectorAll('.item');
+
+console.log(`В списке ${itemRef.length} категории.`);
 
 
+itemRef.forEach(item => {
+    const titleContent = item.firstElementChild.textContent;
+    const totalElements = item.lastElementChild.children.length;
+
+    console.log(`Категория: ${titleContent} \nКоличество елементов: ${totalElements}`);
+
+});
 
